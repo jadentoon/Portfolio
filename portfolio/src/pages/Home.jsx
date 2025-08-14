122b9f38cb6a22ba0e5b7a943fd3b42cf126660f
Jaden Toon <jaden.toon@gmail.com>
2025-08-14 15:32:31 +0100

    Added star background

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import ThemeToggle from '../components/ThemeToggle'
+import Background from '../components/Background'
 
 const Home = () => {
     return (
         <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
             <ThemeToggle/>
+            <Background />
         </div>
     )
 }
